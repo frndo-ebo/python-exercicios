a64d8341b63e36a75664301ee6cec893eb3d6a9e
frndo-ebo <fernando.bessa0301@gmail.com>
2026-02-20 19:51:15 -0300

    Update # Reprodutor de arquivo mp3 (pré-instala.py

--- # Reprodutor de arquivo mp3 (pré-instala.py
+++ # Reprodutor de arquivo mp3 (pré-instala.py
@@ -2,7 +2,7 @@
 
 import pygame
 
-print('Se liga no som do pai')
+print('Vamos ouvir uma música!')
 print('Música tocando... digite "stop" para parar a música')
 while True:
     comando = input('> >').lower().strip()
@@ -14,3 +14,4 @@ pygame.mixer.init()
 pygame.mixer.music.load('c:\\Users\\spbes\\Music\\Downloads\\Eu te devoro remix.mp3')
 pygame.mixer.music.play()
 input()
+
